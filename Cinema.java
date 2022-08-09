16fd1778a8e9b5afe38e12a4f250e70b2109e319
xfoxcom <xfoxcom@gmail.com>
2022-08-09 23:29:29 +0300

    IDE

--- Cinema.java
+++ Cinema.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
+            // Hello fromIDE
             Scanner scr = new Scanner(System.in);
 
             System.out.println("Enter the number of rows: ");
